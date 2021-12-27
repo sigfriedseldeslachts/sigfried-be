e147a669d896c75b3e5538a0f06b9be3dd7b51ba
Sigfried Seldeslachts <sigfriedseldeslachts@gmail.com>
2021-12-27 02:05:03 +0100

    Added inital home page

---
+++ components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <ul>
+      <li><nuxt-link to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/projects">Projects</nuxt-link></li>
+      <li><nuxt-link to="/blog">Blog</nuxt-link></li>
+      <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationComponent'
+}
+</script>
+
+<style lang="scss">
+nav {
+  @apply flex justify-center pt-10 pb-0;
+
+  ul {
+    @apply flex flex-row justify-center w-full text-lg font-bold text-center space-x-2 sm:space-x-16 md:space-x-24;
+
+    li {
+      @apply flex-grow sm:flex-grow-0;
+
+      a {
+        @apply text-gray-100 hover:text-gray-300 transition duration-300 ease-in-out;
+      }
+    }
+  }
+}
+</style>
---
+++ components/sFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="col-span-2">
+      Made by Sigfried with Nuxt
+    </div>
+    <div>
+      <!--<a href="/pgp-key.txt">PGP Key</a>-->
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  @apply grid grid-cols-4 text-sm container mx-auto px-4 py-6;
+
+  &.home {
+    @apply bg-transparent text-gray-200;
+  }
+}
+</style>
