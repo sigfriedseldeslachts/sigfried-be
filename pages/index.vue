32ab6e921a62bf03fa83f335db881b965160a27c
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2022-10-26 22:37:34 +0200

    Removed CV from Index

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,6 @@
             See some projects
             <svg xmlns="http://www.w3.org/2000/svg" class="inline -mt-0.5 ml-2 w-6 stroke-current stroke-2" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/></svg>
           </nuxt-link>
-          <a href="https://cloud.famseldeslachts.be/s/5sCwAHmXYPEydRm">CV (Dutch)</a>
         </div>
       </main>
 
